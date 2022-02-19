3f1e465465a099cea5963c265374a8c6b118fef6
vaarunx <varunrishwandh2001@gmail.com>
2022-02-19 20:46:29 +0530

    Initial commit

---
+++ src/main/java/on_this_day/on_this_day/apiClass.java
@@ -0,0 +1,5 @@
+package on_this_day.on_this_day;
+
+public class apiClass {
+
+}
---
+++ src/test/java/on_this_day/on_this_day/AppTest.java
@@ -0,0 +1,20 @@
+package on_this_day.on_this_day;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
