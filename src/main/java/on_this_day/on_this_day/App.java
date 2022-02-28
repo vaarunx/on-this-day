3a403628fc0b9bc31104dc88f0401d66ace75caf
thuhinkhanna <qor6119@gmail.com>
2022-02-28 17:14:33 +0530

    Added Other Classes in GSON conversion

--- src/main/java/on_this_day/on_this_day/App.java
+++ src/main/java/on_this_day/on_this_day/App.java
@@ -4,12 +4,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.LocalDate;
 import java.util.ArrayList;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -38,14 +35,54 @@ public class App
 			 Selected Conv_obj = gson.fromJson(jSONString, Selected.class);
 			 selected_array.add(i,Conv_obj);
 		}
+		JSONArray event = events.getJSONArray("events");
+		ArrayList<Event> event_array = new ArrayList<Event>();
+		for(int i = 0; i < event.length(); i++) {
+			 JSONObject each_event = event.getJSONObject(i);
+			 String jSONString = each_event.toString();
+			 GsonBuilder builder = new GsonBuilder();
+			 Gson gson = builder.create();
+			 Event Conv_obj = gson.fromJson(jSONString, Event.class);
+			 event_array.add(i,Conv_obj);
+		}
+		JSONArray holiday = events.getJSONArray("holidays");
+		ArrayList<Holiday> holiday_array = new ArrayList<Holiday>();
+		for(int i = 0; i < holiday.length(); i++) {
+			 JSONObject each_holiday = holiday.getJSONObject(i);
+			 String jSONString = each_holiday.toString();
+			 GsonBuilder builder = new GsonBuilder();
+			 Gson gson = builder.create();
+			 Holiday Conv_obj = gson.fromJson(jSONString, Holiday.class);
+			 holiday_array.add(i,Conv_obj);
+		}
+		JSONArray birth = events.getJSONArray("births");
+		ArrayList<Birth> birth_array = new ArrayList<Birth>();
+		for(int i = 0; i < birth.length(); i++) {
+			 JSONObject each_birth = birth.getJSONObject(i);
+			 String jSONString = each_birth.toString();
+			 GsonBuilder builder = new GsonBuilder();
+			 Gson gson = builder.create();
+			 Birth Conv_obj = gson.fromJson(jSONString, Birth.class);
+			 birth_array.add(i,Conv_obj);
+		}
+		JSONArray deaths = events.getJSONArray("deaths");
+		ArrayList<Death> deaths_array = new ArrayList<Death>();
+		for(int i = 0; i < deaths.length(); i++) {
+			 JSONObject each_death = deaths.getJSONObject(i);
+			 String jSONString = each_death.toString();
+			 GsonBuilder builder = new GsonBuilder();
+			 Gson gson = builder.create();
+			 Death Conv_obj = gson.fromJson(jSONString, Death.class);
+			 deaths_array.add(i,Conv_obj);
+		}
 		// Checking
-		for(int i=0; i<selected_array.size();i++) {
-			System.out.println(selected_array.get(i).text); //title
-			System.out.println(selected_array.get(i).year); //year
-			for(int j=0; j<selected_array.get(i).pages.size();j++) {
-				System.out.println(selected_array.get(i).pages.get(j).extract);	//extract of each page
-				System.out.println(selected_array.get(i).pages.get(j).content_urls.desktop.page); // url
-				System.out.println(selected_array.get(i).pages.get(j).thumbnail.source); //image
+		for(int i=0; i<event_array.size();i++) {
+			System.out.println(event_array.get(i).text); //title
+			System.out.println(event_array.get(i).year); //year
+			for(int j=0; j<event_array.get(i).pages.size();j++) {
+				System.out.println(event_array.get(i).pages.get(j).extract);	//extract of each page
+				System.out.println(event_array.get(i).pages.get(j).content_urls.desktop.page); // url
+				System.out.println(event_array.get(i).pages.get(j).thumbnail.source); //image
 			}
 			break;
 		}
