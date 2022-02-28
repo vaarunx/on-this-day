dcbe8610910b0d707240f3d881127320a3d0b705
thuhinkhanna <qor6119@gmail.com>
2022-02-28 19:02:13 +0530

    Added Partial Interface

--- src/main/java/on_this_day/on_this_day/App.java
+++ src/main/java/on_this_day/on_this_day/App.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import com.google.gson.Gson;
@@ -12,6 +11,7 @@ import com.google.gson.GsonBuilder;
 
 public class App 
 {
+	public static TodaysHistoryInfo history = new TodaysHistoryInfo();
 	public static void main(String[] args) {
 		CustomDate date = new CustomDate();
 		date.setDay(27);
@@ -26,66 +26,62 @@ public class App
 	public static String parse(String responseBody) {
 		JSONObject events = new JSONObject(responseBody);
 		JSONArray selected = events.getJSONArray("selected");
-		ArrayList<Selected> selected_array = new ArrayList<Selected>();
 		for(int i = 0; i < selected.length(); i++) {
 			 JSONObject each_selected = selected.getJSONObject(i);
 			 String jSONString = each_selected.toString();
 			 GsonBuilder builder = new GsonBuilder();
 			 Gson gson = builder.create();
 			 Selected Conv_obj = gson.fromJson(jSONString, Selected.class);
-			 selected_array.add(i,Conv_obj);
+			 history.selected_array.add(i,Conv_obj);
 		}
 		JSONArray event = events.getJSONArray("events");
-		ArrayList<Event> event_array = new ArrayList<Event>();
 		for(int i = 0; i < event.length(); i++) {
 			 JSONObject each_event = event.getJSONObject(i);
 			 String jSONString = each_event.toString();
 			 GsonBuilder builder = new GsonBuilder();
 			 Gson gson = builder.create();
 			 Event Conv_obj = gson.fromJson(jSONString, Event.class);
-			 event_array.add(i,Conv_obj);
+			 history.events_array.add(i,Conv_obj);
 		}
 		JSONArray holiday = events.getJSONArray("holidays");
-		ArrayList<Holiday> holiday_array = new ArrayList<Holiday>();
 		for(int i = 0; i < holiday.length(); i++) {
 			 JSONObject each_holiday = holiday.getJSONObject(i);
 			 String jSONString = each_holiday.toString();
 			 GsonBuilder builder = new GsonBuilder();
 			 Gson gson = builder.create();
 			 Holiday Conv_obj = gson.fromJson(jSONString, Holiday.class);
-			 holiday_array.add(i,Conv_obj);
+			 history.holidays_array.add(i,Conv_obj);
 		}
 		JSONArray birth = events.getJSONArray("births");
-		ArrayList<Birth> birth_array = new ArrayList<Birth>();
 		for(int i = 0; i < birth.length(); i++) {
 			 JSONObject each_birth = birth.getJSONObject(i);
 			 String jSONString = each_birth.toString();
 			 GsonBuilder builder = new GsonBuilder();
 			 Gson gson = builder.create();
 			 Birth Conv_obj = gson.fromJson(jSONString, Birth.class);
-			 birth_array.add(i,Conv_obj);
+			 history.births_array.add(i,Conv_obj);
 		}
 		JSONArray deaths = events.getJSONArray("deaths");
-		ArrayList<Death> deaths_array = new ArrayList<Death>();
 		for(int i = 0; i < deaths.length(); i++) {
 			 JSONObject each_death = deaths.getJSONObject(i);
 			 String jSONString = each_death.toString();
 			 GsonBuilder builder = new GsonBuilder();
 			 Gson gson = builder.create();
 			 Death Conv_obj = gson.fromJson(jSONString, Death.class);
-			 deaths_array.add(i,Conv_obj);
+			 history.deaths_array.add(i,Conv_obj);
 		}
 		// Checking
-		for(int i=0; i<event_array.size();i++) {
-			System.out.println(event_array.get(i).text); //title
-			System.out.println(event_array.get(i).year); //year
-			for(int j=0; j<event_array.get(i).pages.size();j++) {
-				System.out.println(event_array.get(i).pages.get(j).extract);	//extract of each page
-				System.out.println(event_array.get(i).pages.get(j).content_urls.desktop.page); // url
-				System.out.println(event_array.get(i).pages.get(j).thumbnail.source); //image
+		/*
+		for(int i=0; i<history.events_array.size();i++) {
+			System.out.println(history.events_array.get(i).text); //title
+			System.out.println(history.events_array.get(i).year); //year
+			for(int j=0; j<history.events_array.get(i).pages.size();j++) {
+				System.out.println(history.events_array.get(i).pages.get(j).extract);	//extract of each page
+				System.out.println(history.events_array.get(i).pages.get(j).content_urls.desktop.page); // url
+				System.out.println(history.events_array.get(i).pages.get(j).originalimage.source); //image
 			}
 			break;
-		}
+		}*/
 		return null;
 	}
 }
--- src/main/java/on_this_day/on_this_day/apiClass.java
+++ src/main/java/on_this_day/on_this_day/apiClass.java
@@ -1,4 +1,7 @@
 package on_this_day.on_this_day;
+
+import java.util.ArrayList;
+
 // Java's default date class is used in the pages class, so this class had to be renamed.
 class CustomDate {
    private int month;
@@ -11,4 +14,54 @@ class CustomDate {
    public int getYear() { return year; }
    public void setYear(int year) { this.year = year; }
 }
+
+class TodaysHistoryInfo {
+
+	ArrayList<Selected> selected_array = new ArrayList<Selected>();
+	ArrayList<Birth> births_array = new ArrayList<Birth>();
+	ArrayList<Death> deaths_array = new ArrayList<Death>();
+	ArrayList<Holiday> holidays_array = new ArrayList<Holiday>();
+	ArrayList<Event> events_array = new ArrayList<Event>();
+	private short year;
+	
+	public short getYear() { return year; }
+	public void setYear(short year) {
+	       this.year = year;
+	   }
+	public ArrayList<Selected> getSelected() {
+	       return selected_array;
+	   }
+	public void setSelected(ArrayList<Selected> S) {
+		this.selected_array = S;
+	}
+	public ArrayList<Event> getEvents() {
+	       return events_array;
+	   }
+	public void setEvents(ArrayList<Event> E) {
+		this.events_array = E;
+	}
+	public ArrayList<Birth> getBirths() {
+	       return births_array;
+	   }
+	public void setBirths(ArrayList<Birth> B) {
+		this.births_array = B;
+	}
+	public ArrayList<Death> getDeaths() {
+	       return deaths_array;
+	   }
+	public void setDeaths(ArrayList<Death> D) {
+		this.deaths_array = D;
+	}
+	public ArrayList<Holiday> getHolidays() {
+	       return holidays_array;
+	   }
+	public void setHolidays(ArrayList<Holiday> H) {
+		this.holidays_array = H;
+	}
+		   
+	}
+interface TodaysHistoryRetriever {
+	   public TodaysHistoryInfo get(CustomDate date);
+	}
+
 public class apiClass { }
